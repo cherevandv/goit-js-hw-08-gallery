aae88c666c6608b682e83b5f26ccfe963611a1a3
Daria Kobekhno <cherevandv@gmail.com>
2020-09-13 20:56:59 +0300

    added left and right swipes

--- js/homework-08.js
+++ js/homework-08.js
@@ -1,87 +1,98 @@
 "use strict";
 
-import galleryItems from './gallery-items.js';
+import galleryItems from "./gallery-items.js";
 
+const galleryRef = document.querySelector(".gallery");
+const lightboxRef = document.querySelector(".lightbox");
+const lightboxImgRef = document.querySelector(".lightbox__image");
+const lightboxBtnRef = document.querySelector(".lightbox__button");
 
-  const galleryRef = document.querySelector(".gallery");
-  const lightboxRef = document.querySelector(".lightbox");
-  const lightboxImgRef = document.querySelector(".lightbox__image");
-  const lightboxBtnRef = document.querySelector(".lightbox__button");
+const createMarkupGallery = (galleryItems, galleryRef) => {
+  const markupGallery = galleryItems.map((item) => {
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+    const image = document.createElement("img");
+    listItem.classList.add("gallery__item");
+    link.classList.add("gallery__link");
+    image.classList.add("gallery__image");
+    link.setAttribute("href", `${item.original}`);
+    image.setAttribute("src", `${item.preview}`);
+    image.setAttribute("data-source", `${item.original}`);
+    image.setAttribute("alt", `${item.description}`);
+    link.appendChild(image);
+    listItem.appendChild(link);
 
+    return listItem;
+  });
+  galleryRef.append(...markupGallery);
 
-  const createMarkupGallery = (galleryItems, galleryRef) => {
-    const markupGallery = galleryItems.map((item)=>{
-        const listItem = document.createElement("li");
-        const link = document.createElement("a");
-        const image = document.createElement("img");
-        listItem.classList.add("gallery__item");
-        link.classList.add("gallery__link");
-        image.classList.add("gallery__image");
-        link.setAttribute("href", `${item.original}`);
-        image.setAttribute("src", `${item.preview}`);    
-        image.setAttribute("data-source", `${item.original}`);
-        image.setAttribute("alt", `${item.description}`);
-        link.appendChild(image);
-        listItem.appendChild(link);
-
-        return listItem;
-    });
-    galleryRef.append(...markupGallery);
-
-    return galleryRef;
+  return galleryRef;
 };
 
 createMarkupGallery(galleryItems, galleryRef);
 
-galleryRef.addEventListener('click', onImageClick);
-// lightboxBtnRef.addEventListener('click',closeModal);
+galleryRef.addEventListener("click", onImageClick);
 
-function onImageClick(event){
-    event.preventDefault();
-    if(event.target.nodeName!=='IMG'){
-        console.log('мимо:)')
-        return;
-    };
-    
-    openModal();
-       
-};
-
-function openModal(){
-    window.addEventListener('keydown', onPressEscape);
-    lightboxRef.classList.add("is-open");
-    lightboxImgRef.setAttribute("src", `${event.target.dataset.source}`);
-    lightboxImgRef.setAttribute("alt", `${event.target.getAttribute('alt')}`);
-};
-
-
-lightboxRef.addEventListener('click', event =>{
-    if(event.target.nodeName === 'BUTTON'){
-        closeModal(); 
-    };
-    if(event.target === event.currentTarget){
-        closeModal(); 
-    };
-
+lightboxRef.addEventListener("click", (event) => {
+  if (
+    event.target.nodeName === "BUTTON" ||
+    event.target === event.currentTarget.children[1]
+  ) {
+    closeModal();
+  }
 });
 
-function closeModal(){    
-    window.removeEventListener('keydown', onPressEscape);
-        lightboxRef.classList.remove("is-open");
-        lightboxImgRef.setAttribute("src", ``);
-        lightboxImgRef.setAttribute("alt", ``);
-    };
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  openModal();
+}
 
-function onPressEscape (event){
-  if(event.code === 'Escape'){
+function openModal() {
+  window.addEventListener("keydown", onPressEscape);
+  window.addEventListener("keydown", onPressArrowKeys);
+
+  lightboxRef.classList.add("is-open");
+  lightboxImgRef.setAttribute("src", `${event.target.dataset.source}`);
+  lightboxImgRef.setAttribute("alt", `${event.target.getAttribute("alt")}`);
+}
+
+function closeModal() {
+  window.removeEventListener("keydown", onPressEscape);
+  window.removeEventListener("keydown", onPressArrowKeys);
+
+  lightboxRef.classList.remove("is-open");
+  lightboxImgRef.setAttribute("src", ``);
+  lightboxImgRef.setAttribute("alt", ``);
+}
+
+function onPressEscape(event) {
+  if (event.code === "Escape") {
     closeModal();
-  };
-};
+  }
+}
 
-// function closeModal(){    
-// if(event.target.nodeName === 'BUTTON'){
-//     lightboxRef.classList.remove("is-open");
-//     lightboxImgRef.setAttribute("src", ``);
-//     lightboxImgRef.setAttribute("alt", ``);
-// }
-// };
+function onPressArrowKeys(event) {
+  let index = galleryItems.indexOf(
+    galleryItems.find(
+      (item) => item.original === lightboxImgRef.getAttribute("src")
+    )
+  );
+
+  if (event.code === "ArrowRight") {
+    if (index === galleryItems.length - 1) {
+      return;
+    }
+    index += 1;
+  }
+  if (event.code === "ArrowLeft") {
+    if (index === 0) {
+      return;
+    }
+    index -= 1;
+  }
+  lightboxImgRef.setAttribute("src", `${galleryItems[index].original}`);
+  lightboxImgRef.setAttribute("alt", `${galleryItems[index].description}`);
+}
